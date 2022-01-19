19bcbfebe7440a4ad313bf3cf667e0ecc3b6bf8f
ryo0705s <inspire.sasaki15@gmail.com>
2022-01-19 12:58:33 +0900

    ページを作成

--- work/public/index.php
+++ work/public/index.php
@@ -1,3 +1,10 @@
+<?php
+function h($str)
+{
+  return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+$todos = filter_input(INPUT_POST, "content");
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -6,5 +13,17 @@
 </head>
 <body>
   <h1>Todos</h1>
+  <p>やること</p>
+  <ul>
+    <?php foreach($todos as $todo): ?>
+      <li><?= h($todo) ?></li>
+      <?php endforeach; ?>
+    </ul>
+    
+  <form action="" method="post">
+  <input type="text" name="content">
+  <button>追加</button>
+  </form>
+
 </body>
 </html>
