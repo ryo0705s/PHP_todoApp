be650904edefc1da29e1c784e100c333bc519998
ryo0705s <inspire.sasaki15@gmail.com>
2022-01-25 15:30:12 +0900

    todo作成機能完成＆削除機能実装中

--- work/public/index.php
+++ work/public/index.php
@@ -1,9 +1,39 @@
 <?php
+
+// require('./main.js');
 function h($str)
 {
   return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
-$todos = filter_input(INPUT_POST, "content");
+// $hoge = [];
+$todo = [];
+$todos = [];
+$fileName = '../app/data.txt';
+if (!empty($_POST['content'])){
+  $fp = fopen($fileName,'a');
+  $todo = $_POST['content'];
+  // $todo = filter_input(INPUT_POST, 'content');
+  fwrite($fp, $todo . "\n");
+  fclose($fp);
+  $todos = file($fileName, FILE_IGNORE_NEW_LINES);
+}
+if (isset($_POST['del'])){
+  $fp = fopen($fileName,'w');
+  foreach($todos as $key => $newtodos){
+    if($key != $_POST['del']){
+      fwrite($fp, $newtodos."\n");
+  }
+  }
+  fclose($fp);
+}
+// if(isset($_POST['del'])){
+//   $fp = fopen("data.txt", 'w');
+//   foreach($BOARD as $key => $NEWBOARD){
+//       if($key != $_POST['del']){
+//           fwrite($fp, $NEWBOARD."\n");
+//       }
+//   }
+//   fclose($fp);
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -15,8 +45,13 @@ $todos = filter_input(INPUT_POST, "content");
   <h1>Todos</h1>
   <p>やること</p>
   <ul>
-    <?php foreach($todos as $todo): ?>
+    <?php foreach($todos as $key => $todo): ?>
+      <li><?= h($key) ?></li>
       <li><?= h($todo) ?></li>
+      <form action="" method="post">
+  <input type="" name="del" value="<?=$key; ?>">
+  <button>削除</button>
+  </form>
       <?php endforeach; ?>
     </ul>
     
---
+++ work/public/main.js
@@ -0,0 +1,12 @@
+"use strict";
+{
+  const deletes = document.querySelectorAll(button);
+  deletes.addEventListener("click", () => {});
+
+  // const delete = document.querySelectorAll('.delete');
+  // deletes.forEach(span => {
+  //   span.addEventListener('click', () => {
+  //     span.parentNode.submit();
+  //   });
+  // });
+}
